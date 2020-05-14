7fcc7dffb939199949d7c2cd962de6059e7883ae
ruslan <sqwer1986@gmail.com>
2020-05-15 00:07:39 +0300

    number_of_letters

---
+++ lesson1/number_of_letters.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const vowels = 'aeiouy';
+const consonants = 'bcdfghjklmnpqrstvwxz';
+let vowelsCount = 0, consonantsCount = 0;
+
+let string = prompt('Enter string');
+
+string = string.replace(/(\t+|\s+)/g, '').toLowerCase();
+
+for (let i = 0; i < string.length; i++) {
+  if (vowels.indexOf(string[i]) >= 0) vowelsCount++;
+
+  if (consonants.indexOf(string[i]) >= 0) consonantsCount++;
+}
+
+alert(`Количество гласных букв: ${vowelsCount}\n Количество согласных букв: ${consonantsCount}`)
