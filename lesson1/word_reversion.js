5b6953bfdeffd9b996acf52f847f732bfd41a77a
ruslan <sqwer1986@gmail.com>
2020-05-15 01:17:01 +0300

    change word_reversion

--- lesson1/word_reversion.js
+++ lesson1/word_reversion.js
@@ -1,9 +1,18 @@
 'use strict'
 
+// let word = prompt('Введите слово', '');
+
+// let reversWord = [];
+
+// for (let i = word.length; i != -1 ; i--) reversWord.push(word[i]);
+
+// alert(reversWord.join(''))
+
+
+// Более простой вариант
+
 let word = prompt('Введите слово', '');
 
-let reversWord = [];
+let reversWord = word.split('').reverse().join('');
 
-for (let i = word.length; i != -1 ; i--) reversWord.push(word[i]);
-
-alert(reversWord.join(''))
+alert(reversWord)
