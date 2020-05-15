0792063f0053534804ee32d2d5bdcfbc0882b590
ruslan <sqwer1986@gmail.com>
2020-05-15 22:34:00 +0300

    bug fix

--- lesson1/average.js
+++ lesson1/average.js
@@ -5,13 +5,13 @@ let sum;
 let average;
 
 while (true) {
-	let number = +prompt("Введите число", '');
+    let number = prompt("Введите число", '');
 
-	if (!number) break;
+    if (number == '') break;
 
-	numbers.push(number);
-	average = (sum = numbers.reduce((a, b) => a + b, 0)) / numbers.length;
-	console.log(`Сумма чисел = ${sum}\n количество чисел = ${numbers.length}\n среднее арифметическое = ${average}\n`);
+    numbers.push(+number);
+    average = (sum = numbers.reduce((a, b) => a + b, 0)) / numbers.length;
+    console.log('Сумма чисел = ' + sum + '\n количество чисел = ' + numbers.length + '\n среднее арифметическое = ' + average + '\n');
 }
 
 alert(`Сумма чисел = ${sum}\n количество чисел = ${numbers.length}\n среднее арифметическое = ${average}\n`)
--- lesson1/identifier_encoding_algorithm.js
+++ lesson1/identifier_encoding_algorithm.js
@@ -5,16 +5,38 @@
 // C. тип
 // D. идентификатор пользователя
 // Все входные данные - целые десятичные числа (значения можно “зашить” прямо в код решения).
+// const id = '5eabb7d03a854bdff'
+
+// const a = id.substr(0, 8);
+// const timestamp = parseInt(a, 16);
+
+// const b = id.substr(8, 2);
+// const claster = parseInt(b, 16);
+
+// const c = id.substr(10, 1);
+// const type = parseInt(c, 16);
+
+// const d = id.substr(11);
+// const user = parseInt(d, 16);
+
+
+// console.log(a, b, c, d);
+// console.log('timestamp :', timestamp, new Date(timestamp * 1000));
+// console.log('claster :', claster);
+// console.log('type :', type);
+// console.log('user :', user);
+// ======================================================================
+
 'use strict'
 
-const a = 3473276432;
+const a = 1588312016;
 
-const b = 10;
+const b = 58;
 
 const c = 8;
 
-const d = 77777777;
+const d = 5553663;
 
-let id = a.toString(16) + b.toString(16) + c.toString(16) + d.toString(16);
+let id = a.toString(16) + (b <= 10 ? '0' + b.toString(16) : b.toString(16)) + c.toString(16) + d.toString(16);
 
 alert(id)
