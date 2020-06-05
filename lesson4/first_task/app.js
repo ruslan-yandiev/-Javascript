c67872072b755386cc3d81e0ca1c723563316d4b
ruslan <sqwer1986@gmail.com>
2020-06-05 21:47:13 +0300

    create function showResult

--- lesson4/first_task/app.js
+++ lesson4/first_task/app.js
@@ -1,19 +1,19 @@
 'use strict'
 
 function showDomTree1(node = document, step = 0) {
-    console.log(' '.repeat(step), node.nodeName, chengeNull(node.nodeValue));
+    showResult(node, step);
 
     for (let child of node.childNodes) showDomTree1(child, step + 6);
 }
 
 function showDomTree2(node = document, step = 0) {
-    console.log(' '.repeat(step), node.nodeName, chengeNull(node.nodeValue));
+    showResult(node, step);
 
     Array.from(node.childNodes).forEach((elem) => showDomTree2(elem, step + 6));
 }
 
 function showDomTree3(node = document, step = 0) {
-    console.log(' '.repeat(step), node.nodeName, chengeNull(node.nodeValue));
+    showResult(node, step);
 
     for (let i = 0; i < node.childNodes.length; i++) {
         let child = node.childNodes[i];
@@ -23,13 +23,13 @@ function showDomTree3(node = document, step = 0) {
 
 function showDomTree4(node = document, step = 0) {
     while (node) {
-        console.log(' '.repeat(step), node.nodeName, chengeNull(node.nodeValue));
+        showResult(node, step);
         showDomTree4(node.firstChild, step + 6);
         node = node.nextSibling;
     };
 }
 
-function chengeNull(nodeValue) { return nodeValue ? nodeValue : '' }
+function showResult(node, step) { console.log(' '.repeat(step), node.nodeName, node.nodeValue || '') }
 
 showDomTree1(); 
 showDomTree2();
