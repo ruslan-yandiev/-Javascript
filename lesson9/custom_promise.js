6d900b535ca807eb453f2c9ce990add12c093fc5
ruslan <sqwer1986@gmail.com>
2020-07-27 23:11:47 +0300

    реализация создания выполненного промиса

--- lesson9/custom_promise.js
+++ lesson9/custom_promise.js
@@ -1,10 +1,9 @@
 'use strict'
-
 class CustomPromise {
     constructor(callback) {
         this.__success__ = []
         this.__error__ = []
-        this._callback = callback;
+        this._callback = callback
         this._status = 'pending'
 
         if (callback) {
@@ -23,6 +22,16 @@ class CustomPromise {
             this.__error__.push(rejectCb)
         }
 
+        if (this._status === 'fulfilled') {
+            this.__success__.forEach(cb => cb(this.result));
+            this.__success__ = [];
+        };
+
+        if (this._status === 'rejected') {
+            this.__error__.forEach(cb => cb(this.result));
+            this.__error__ = [];
+        };
+
         return this;
     }
 
@@ -31,30 +40,45 @@ class CustomPromise {
     }
 
     _resolve(result) {
-        this._status = 'fulfilled'
-        this.__success__.forEach(cb => cb(result))
+        this.__success__.forEach(cb => cb(result));
+        this._status = 'fulfilled';
     }
 
     _reject(err) {
-        this._status = 'rejected'
-        this.__error__.forEach(cb => cb(err))
+        this.__error__.forEach(cb => cb(err));
+        this._status = 'rejected';
+    }
+
+    static resolve(result) {
+        const promise = new CustomPromise();
+        promise.result = result;
+        promise._resolve(result);
+        return promise;
+    }
+
+    static reject(err) {
+        const promise = new CustomPromise();
+        promise.result = err;
+        promise._reject(err);
+        return promise;
     }
 };
 
-let promise = new CustomPromise((resolve, reject) => {
-    resolve('this is a promise');
+
+const promise = new CustomPromise((resolve, reject) => {
+    resolve({ promis: 'this is a promise'});
 
     setTimeout(() => {
         reject('Ошибочка')
     }, 1000)
 });
 
-promise.then(console.log('Hi')).then(console.log(1986)).then(title => console.log('MyPrpmise:', title));
+promise.then(console.log('Hi')).then(console.log(1986)).then(obj => console.log('MyPrpmise:', obj)).then(title => console.log(title.promis));
 promise.catch((err) => {
     console.log('Rejected', err)
 });
 
-let promise2 = new CustomPromise((resolve, reject) => {
+const promise2 = new CustomPromise((resolve, reject) => {
     setTimeout(() => {
         resolve(123)
     }, 1000)
@@ -64,5 +88,8 @@ promise2.then((resolve) => {
     console.log('Result', resolve)
 }).then(console.log);
 
-// promise2.then(console.log);
+const promise4 = CustomPromise.resolve(777);
+promise4.then(i => console.log(i));
 
+const promise5 = CustomPromise.reject('Ошибка промиса5');
+promise5.catch(err => console.log(err));
