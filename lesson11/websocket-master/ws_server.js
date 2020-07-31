356b25b7118437b8c4d953f222872faa9e760e47
ruslan <sqwer1986@gmail.com>
2020-07-31 21:01:59 +0300

    download repository

---
+++ lesson11/websocket-master/ws_server.js
@@ -0,0 +1,45 @@
+const WebSocket = require("ws");
+const wsConnection = new WebSocket.Server({ port: 8080 });
+
+const clients = new Set();
+
+class User {
+  constructor(connection) {
+    this.connection = connection;
+    this._channels = new Set();
+  }
+
+  loginChannel(channelName) {}
+
+  logoutChannel(channelName) {}
+
+  isLoggedIn(channelName) {}
+}
+
+wsConnection.on("connection", ws => {
+  const user = new User(ws);
+  clients.add(user);
+
+  user.connection.on("message", function(data) {
+    const message = JSON.parse(data);
+
+    switch (message.command) {
+      case "login":
+        return;
+      case "logout":
+        return;
+      case "sendMessage":
+        return;
+      case "exitChat":
+        return;
+      default:
+        ws.send("Unknown command");
+        return;
+    }
+  });
+
+  user.connection.on("close", function() {
+    clients.delete(user);
+  });
+
+});
